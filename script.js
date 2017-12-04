e1c11b777d7df516c6a7c2532c1d7ebc453fd61a
Jesse Stolwijk <stolwijkjesse@gmail.com>
2017-12-04 22:04:43 +0100

    fixed bug youtube fullscreen updates slow

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 
 document.body.style.border = "5px solid red";
 var vids = document.getElementsByTagName('video')
+console.log(vids.length);
 var video = vids[0];
 console.log("DOM loaded");
 var fullscreen = window.innerHeight == window.screen.height && window.innerWidth == window.screen.width;
 document.addEventListener("mozfullscreenchange", function( event ) {
     fullscreen = window.innerHeight == window.screen.height && window.innerWidth == window.screen.width;
-    console.log("fullscreenchangeevenmt!!!");    
-    if (fullscreen) {
-        console.log("Fullscreen");
-        video.style.width = "2560px";
-        video.style.left = 0;
-        video.style.height = "1440px";
-        video.style.top = "-180px";
-    } else {
-        video.style.border = "";
-    }
+    console.log("fullscreenchangeevenmt!!!");   
+    setTimeout(function(){
+        if (fullscreen||window.fullScreen) {
+            console.log("Fullscreen");
+            video.style.width = window.screen.width + "px";
+            video.style.left = 0;
+            video.style.height = window.screen.width / 16 * 9 + "px";
+            video.style.top = (window.screen.height - window.screen.width / 16 * 9) / 2 + "px";
+        } else {
+            video.style = "";
+        }    }, 200);
+    
 });
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
