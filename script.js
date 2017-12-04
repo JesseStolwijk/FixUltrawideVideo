67f1801a2511fa7a5a28838a29c831508207d6cb
Jesse Stolwijk <stolwijkjesse@gmail.com>
2017-12-04 22:34:09 +0100

    Manifest updated

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ console.log("DOM loaded");
 var fullscreen = window.innerHeight == window.screen.height && window.innerWidth == window.screen.width;
 document.addEventListener("mozfullscreenchange", function( event ) {
     fullscreen = window.innerHeight == window.screen.height && window.innerWidth == window.screen.width;
-    console.log("fullscreenchangeevenmt!!!");   
     setTimeout(function(){
         if (fullscreen||window.fullScreen) {
             console.log("Fullscreen");
@@ -17,15 +16,10 @@ document.addEventListener("mozfullscreenchange", function( event ) {
             video.style.top = (window.screen.height - window.screen.width / 16 * 9) / 2 + "px";
         } else {
             video.style = "";
-        }    }, 200);
-    
+        }    
+    }, 200);  
 });
-var canvas = document.createElement("canvas");
-var ctx = canvas.getContext("2d");
-ctx.drawImage(video,0,0,256,256);  
-document.body.appendChild(canvas); 
-//
-//video.style.left = 0;
-//video.style.top = -100+"px";
-
-console.log("Jesse: " + document.body.clientWidth);
+//var canvas = document.createElement("canvas");
+//var ctx = canvas.getContext("2d");
+//ctx.drawImage(video,0,0,256,256);  
+//document.body.appendChild(canvas); 
